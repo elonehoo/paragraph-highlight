69bb6405e2048f9683bea123f0b40a5a0d0ef8a1
xiaoxunyao <huchengyea@163.com>
2022-04-11 16:35:30 +0800

    fix: update core

--- example/vue-demo/src/pages/index.vue
+++ example/vue-demo/src/pages/index.vue
@@ -1,50 +1,54 @@
 <script setup lang="ts">
-const name = ref('')
+import paragraphHighlight from '@elonehoo/paragraph-highlight/src'
 
-const router = useRouter()
-const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
+const content = "Tropical birds scattered as Drake veered the Jeep onto an old rutted track, snapping branches and tearing away vines, plowing through the rain forest with killers in pursuit, bullets flying, a gorgeous but pouty girl in the passenger's seat, and a bitch of a headache. . With only one of his arms on the wheel, the Jeep slewed to the left, and the pouty girl screamed as he forced the vehicle back onto the trail just before they would have crashed into a felled tree."+
+"Tropical birds scattered as Drake swerved the Jeep onto a rutted old track, snapping branches and ripping vines, hurtling through the rain forest with killers in pursuit, bullets flying, and a beautiful but pouting girl in the passenger seat with headache. With only one arm on the wheel, the Jeep veered to the left, and the grumpy girl screamed, forcing the vehicle back onto the road just before they slammed into a felled tree."
+
+const query = ref('Drak')
+
 </script>
 
 <template>
   <div>
-    <img src="/favicon.svg" alt="logo" text-4xl inline-block>
-    <p>
-      <a rel="noreferrer" href="https://github.com/xiaoxunyao/vitetem-mini" target="_blank">
-        Vitetem Mini
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
+    <h1>
+      <paragraphHighlight highlight-tag="text" query="highlight">paragraph highlight</paragraphHighlight>
+    </h1>
+    <div class="card">
+      <input
+        id="input"
+        v-model="query"
+        type="text"
+        autocomplete="false"
+        p="x-4 y-2"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
       >
-        Go
-      </button>
+      <br />
+      Query
+      <br />
+      <paragraphHighlight :query="query">{{content}}</paragraphHighlight>
     </div>
   </div>
 </template>
+
+<style>
+.card {
+  width: 500px;
+  max-width: 100vw;
+  margin: auto;
+  margin-top: 40px;
+  /* background: white; */
+  padding: 20px;
+  padding-bottom: 30px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  box-shadow: 0 20px 50px rgba(0, 0, 0, .1);
+}
+text{
+  background-color: #42b983;
+  color: #FFFFFFF0;
+}
+</style>
